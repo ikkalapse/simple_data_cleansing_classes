5a97a7fe5a82945dbcc03c19bcd2fe0426663613
Victoria Zelepukhina <ikkalapse@gmail.com>
2021-07-30 15:09:59 +0400

    Projects parent directory deleted

--- simple_data_cleansing_classes/project.py
+++ simple_data_cleansing_classes/project.py
@@ -3,18 +3,12 @@ import os
 
 class Project:
 
-    name = None  # Project name and project directory name
-
-    project_parent_dir = None  # Project parent directory path
-
     project_dir = None  # Project directory path
 
     sub_dirs = None   # Subdirectories included into the project directory
 
-    def __init__(self, project_name, project_parent_dir, sub_dirs=None):
-        self.name = project_name  # Project name
-        self.project_parent_dir = project_parent_dir  # Project parent directory
-        self.project_dir = os.path.abspath(os.path.join(self.project_parent_dir, self.name))  # Project directory
+    def __init__(self, project_dir, sub_dirs=None):
+        self.project_dir = os.path.abspath(project_dir)  # Project directory
         os.makedirs(self.project_dir, exist_ok=True)
         try:
             self.sub_dirs = {}
@@ -34,9 +28,7 @@ class Project:
 
 if __name__ == '__main__':
 
-    test_project = Project("__test_dupl__",
-                           os.path.join('..', 'projects'),
-                           ['datasets', 'tests'])
+    test_project = Project(os.path.join("..", "projects", "test"), ['datasets', 'tests'])
     print(test_project.project_dir)
     print(test_project.datasets_dir)
     print(test_project.tests_dir)
